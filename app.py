70da81bc51cdfa2032e6df7ac37aab25990f079d
minhlv <lvminh2k@gmail.com>
2024-04-23 16:41:10 +0700

    add method

--- app.py
+++ app.py
@@ -7,13 +7,23 @@ def hello():
 
 account_list = [
     {"id": 1, "username" : "huong", "password": 1234},
-    {"id": 2 , "username" : "thuy", "password" : 5678}
+    {"id": 2 , "username" : "thuy", "password" : 5678},
 ]
 
 @app.route("/get_account_list")
 def get_account_list():
     return jsonify(account_list)
-    
+
+@app.route("/add_account", method = ['POST'])
+def add_account():
+    id = request.json['id']
+    username = request.json['username']
+    password = request.json['password']
+    user =  {"id": id, "username" : username, "password": password}
+    account_list.append(user)
+    return {"message":"add user with id" + str(id)}
+
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8080)
 
