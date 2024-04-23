d83c46a15587aa172461bef28037a93da5cf0180
minhlv <lvminh2k@gmail.com>
2024-04-23 16:21:17 +0700

    add method

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8080)
 
 account_list = [
-    {"id": 1, "username" : "huong", "password": 1234}
-    {"id":2 , "username" : "thuy", "password" : 5678}
+    {"id": 1, "username" : "huong", "password": 1234},
+    {"id": 2 , "username" : "thuy", "password" : 5678}
 ]
 @app.route("/get_account_list")
 def get_account_list():
