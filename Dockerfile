54cb5579c99169b3354f45dec4048fa748939ac0
minhlv <lvminh2k@gmail.com>
2024-04-23 16:19:23 +0700

    add method

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.7-slim
 
 RUN apt-get update
 RUN apt-get install -y python3-dev
-
+#
 WORKDIR /app
 #
 COPY requirements.txt /app
--- app.py
+++ app.py
@@ -6,3 +6,11 @@ def hello():
     return "my name is Anh Huong"
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8080)
+
+account_list = [
+    {"id": 1, "username" : "huong", "password": 1234}
+    {"id":2 , "username" : "thuy", "password" : 5678}
+]
+@app.route("/get_account_list")
+def get_account_list():
+    return jsonify(account_list)
