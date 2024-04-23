fcb8c83bb66098b94d024c73083a5967f4ad5c14
minhlv <lvminh2k@gmail.com>
2024-04-23 16:24:13 +0700

    add method

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ app = Flask (__name__)
 @app.route("/")
 def hello():
     return "my name is Anh Huong"
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8080)
 
 account_list = [
     {"id": 1, "username" : "huong", "password": 1234},
     {"id": 2 , "username" : "thuy", "password" : 5678}
 ]
+
 @app.route("/get_account_list")
 def get_account_list():
     return jsonify(account_list)
+    
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 8080)
+
